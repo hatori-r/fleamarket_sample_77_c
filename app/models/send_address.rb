139bdb27ddfe01390e4ad41e414882fb4fabfd95
tessy-man1120 <k.tessyma1120@gmail.com>
2020-06-23 10:43:03 +0900

    wizard_create_kt

--- app/models/send_address.rb
+++ app/models/send_address.rb
@@ -1,3 +1,6 @@
 class SendAddress < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
+  validates :post_number, :prefectures, :city, :address, presence: true
+  validates :send_family_name, :send_first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }, presence: true
+  validates :send_family_kana, :send_first_kana, format: { with: /\A[ぁ-んー－]+\z/ }, presence: true
 end
