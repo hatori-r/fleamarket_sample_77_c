2fd9571b3ca62631fa50bbeb671cde1adb677a7d
ADRIAN ZULNEDI <54824443+adrian-zulnedi@users.noreply.github.com>
2020-06-24 12:02:50 +0900

    mypage-01-commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  private
+
+  def users_params
+    params.require(:user).permit(:nickname, :password, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday, :image, :introduction, :user_id)
+  end
+end
